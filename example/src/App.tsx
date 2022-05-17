bf919c1095abc7b50cbd3da032fcf31dccdafdee
PechaRichard <76245185+PechaRichard@users.noreply.github.com>
2022-05-17 12:16:38 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'use-amplify'
+import 'use-amplify/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
